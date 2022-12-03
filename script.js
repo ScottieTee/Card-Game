aa7c9860a8285b5183c3dd478be8508ef4389e80
Sarah Thompson <sscotthompson@gmail.com>
2022-12-03 13:26:22 -0500

    game logic begin

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const deck = new Deck()
 deck.shuffle()
 console.log(deck.cards)
 
+//this will cover player decks * howevermany players
+let playerDecks
+
+startGame()
+function startGame() {
+    const deck = new Deck()
+    deck.shuffle()
+
+    //i need to create a numberOfPlayers variable
+    //ceil prevents a rounding error
+    const deckSplit = Math.ceil(deck.numberOfCards / numberOfPlayers)
+    playerDecks = new Deck(deck.cards.slice(0, deckSplit))
+
+    console.log(playerDecks)
+
+}
