bf9af9fbf3f68fe1da77a84c53f96c80bea719b0
Sarah Thompson <sscotthompson@gmail.com>
2022-12-07 00:30:11 -0500

    clean up

--- node.js
+++
@@ -1,182 +0,0 @@
-Script started on Mon Dec  5 09:33:49 2022
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode \ .js[?2004l
-]633;C]633;E;node.jszsh: command not found: node.js
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;127]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.js[s[2K[0G6[2G
-0 
-1 
-2 [s[2K[0G2 3[4G[s[2K[0G2 3h[5G[s[2K[0G2 3h [6G[s[2K[0G2 3h 3[7G[s[2K[0G2 3h 3d[8G[s[2K[0G2 3h 3d [9G[s[2K[0G2 3h 3d 3[10G[s[2K[0G2 3h 3d 3 [11G[s[2K[0G2 3h 3d 3 c[12G[2D[s[2K[0G2 3h 3d 3 [11G[2D[s[2K[0G2 3h 3d 3[10G[s[2K[0G2 3h 3d 3c[11G
-3 
-4 
-5 
-file:///Users/scotthompson/Desktop/Card-Game/hand.js:63
-        return 10000 * (this.cards[0].rank == this.cards[1].rank && this.cards[2].rank == this.cards[1].rank)
-                                                            ^
-
-TypeError: Cannot read properties of undefined (reading 'rank')
-    at Hand.hasThreeOfKind (file:///Users/scotthompson/Desktop/Card-Game/hand.js:63:61)
-    at Hand.getHandValue (file:///Users/scotthompson/Desktop/Card-Game/hand.js:100:18)
-    at new Hand (file:///Users/scotthompson/Desktop/Card-Game/hand.js:54:27)
-    at file:///Users/scotthompson/Desktop/Card-Game/script.js:18:39
-    at Array.map (<anonymous>)
-    at file:///Users/scotthompson/Desktop/Card-Game/script.js:18:23
-[90m    at ModuleJob.run (node:internal/modules/esm/module_job:197:25)[39m
-    at async Promise.all (index 0)
-[90m    at async ESMLoader.import (node:internal/modules/esm/loader:337:24)[39m
-[90m    at async loadESM (node:internal/process/esm_loader:88:5)[39m
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;1]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004h[?2004l
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;C]633;E;]633;D]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.js[s[2K[0G6[2G
-0 [s[2K[0G0 Qc [6G[6G[s[2K[0G0 Qc Kc [9G[9G[s[2K[0G0 Qc Kc 4s[11G[11G
-1 [s[2K[0G1 Ah [6G[6G[s[2K[0G1 Ah 2c [9G[9G[s[2K[0G1 Ah 2c Js[11G[11G
-2 [s[2K[0G2 3h [6G[6G[s[2K[0G2 3h 9h [9G[9G[s[2K[0G2 3h 9h Th[11G[11G
-3 [s[2K[0G3 Tc [6G[6G[s[2K[0G3 Tc 9c [9G[9G[s[2K[0G3 Tc 9c 3c[11G[11G
-4 [s[2K[0G4 K[4G[s[2K[0G4 Ks[5G[s[2K[0G4 Ks [6G[s[2K[0G4 Ks 5[7G[s[2K[0G4 Ks 5d[8G[s[2K[0G4 Ks 5d [9G[s[2K[0G4 Ks 5d 4[10G[s[2K[0G4 Ks 5d 4h[11G
-5 [s[2K[0G5 Q[4G[s[2K[0G5 Qd[5G[s[2K[0G5 Qd [6G[s[2K[0G5 Qd K[7G[s[2K[0G5 Qd Kd[8G[s[2K[0G5 Qd Kd [9G[s[2K[0G5 Qd Kd A[10G[s[2K[0G5 Qd Kd Ad[11G
-5
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.js[s[2K[0G2[2G
-0 [s[2K[0G0 Ah [6G[6G[s[2K[0G0 Ah 2c [9G[9G[s[2K[0G0 Ah 2c Js[11G[11G
-1 [s[2K[0G1 3h [6G[6G[s[2K[0G1 3h 9h [9G[9G[s[2K[0G1 3h 9h Th[11G[11G
-1
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.jsHow many players?[s[2K[0GHow many players?3[19G
-0 
-1 
-2 
-file:///Users/scotthompson/Desktop/Card-Game/hand.js:63
-        return 10000 * (this.cards[0].rank == this.cards[1].rank && this.cards[2].rank == this.cards[1].rank)
-                                                            ^
-
-TypeError: Cannot read properties of undefined (reading 'rank')
-    at Hand.hasThreeOfKind (file:///Users/scotthompson/Desktop/Card-Game/hand.js:63:61)
-    at Hand.getHandValue (file:///Users/scotthompson/Desktop/Card-Game/hand.js:100:18)
-    at new Hand (file:///Users/scotthompson/Desktop/Card-Game/hand.js:54:27)
-    at file:///Users/scotthompson/Desktop/Card-Game/script.js:16:39
-    at Array.map (<anonymous>)
-    at file:///Users/scotthompson/Desktop/Card-Game/script.js:16:23
-[90m    at ModuleJob.run (node:internal/modules/esm/module_job:197:25)[39m
-    at async Promise.all (index 0)
-[90m    at async ESMLoader.import (node:internal/modules/esm/loader:337:24)[39m
-[90m    at async loadESM (node:internal/process/esm_loader:88:5)[39m
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;1]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.jsHow many players?[s[2K[0GHow many players?2[19G
-0 [s[2K[0G0 4[4G[s[2K[0G0 4h[5G[s[2K[0G0 4h5[6G[s[2K[0G0 4h5d[7G[s[2K[0G0 4h5d6[8G[s[2K[0G0 4h5d6c[9G[1D[1D[s[2K[0G0 4h5d 6c[8G[1D[1D[1D[s[2K[0G0 4h 5d 6c[6G[7G[8G[9G[10G[11G
-1 [s[2K[0G1 3[4G[s[2K[0G1 3c[5G[s[2K[0G1 3c [6G[s[2K[0G1 3c 4[7G[s[2K[0G1 3c 4c[8G[s[2K[0G1 3c 4c [9G[s[2K[0G1 3c 4c 5[10G[s[2K[0G1 3c 4c 5c[11G
-1
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hggit add .[?2004l
-]633;C]633;E;git add .[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hggit commit -m "final version of card game"[?2004l
-]633;C]633;E;git commit -m "final version of card game"[main e5f968a] final version of card game
- 8 files changed, 256 insertions(+), 225 deletions(-)
- rewrite Xdeck.js (85%)
- rewrite hand.js (95%)
- create mode 100644 node.js
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hggit oush iri       push origin a main[?2004l
-]633;C]633;E;git push origin mainEnumerating objects: 18, done.
-Counting objects:   5% (1/18)Counting objects:  11% (2/18)Counting objects:  16% (3/18)Counting objects:  22% (4/18)Counting objects:  27% (5/18)Counting objects:  33% (6/18)Counting objects:  38% (7/18)Counting objects:  44% (8/18)Counting objects:  50% (9/18)Counting objects:  55% (10/18)Counting objects:  61% (11/18)Counting objects:  66% (12/18)Counting objects:  72% (13/18)Counting objects:  77% (14/18)Counting objects:  83% (15/18)Counting objects:  88% (16/18)Counting objects:  94% (17/18)Counting objects: 100% (18/18)Counting objects: 100% (18/18), done.
-Delta compression using up to 2 threads
-Compressing objects:  11% (1/9)Compressing objects:  22% (2/9)Compressing objects:  33% (3/9)Compressing objects:  44% (4/9)Compressing objects:  55% (5/9)Compressing objects:  66% (6/9)Compressing objects:  77% (7/9)Compressing objects:  88% (8/9)Compressing objects: 100% (9/9)Compressing objects: 100% (9/9), done.
-Writing objects:  10% (1/10)Writing objects:  20% (2/10)Writing objects:  30% (3/10)Writing objects:  40% (4/10)Writing objects:  50% (5/10)Writing objects:  60% (6/10)Writing objects:  70% (7/10)Writing objects:  80% (8/10)Writing objects:  90% (9/10)Writing objects: 100% (10/10)Writing objects: 100% (10/10), 3.44 KiB | 3.44 MiB/s, done.
-Total 10 (delta 4), reused 0 (delta 0), pack-reused 0
-remote: Resolving deltas:   0% (0/4)[Kremote: Resolving deltas:  25% (1/4)[Kremote: Resolving deltas:  50% (2/4)[Kremote: Resolving deltas:  75% (3/4)[Kremote: Resolving deltas: 100% (4/4)[Kremote: Resolving deltas: 100% (4/4), completed with 4 local objects.[K
-To github.com:ScottieTee/Card-Game.git
-   26924c0..e5f968a  main -> main
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hggit add .[?2004l
-]633;C]633;E;git add .[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hggit commit - m"  "something extra"[?2004l
-]633;C]633;E;git commit -m "something extra"[main 64d1d5a] something extra
- 3 files changed, 21 insertions(+)
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hggit ouh   o puh sh origin main[?2004l
-]633;C]633;E;git push origin mainEnumerating objects: 9, done.
-Counting objects:  11% (1/9)Counting objects:  22% (2/9)Counting objects:  33% (3/9)Counting objects:  44% (4/9)Counting objects:  55% (5/9)Counting objects:  66% (6/9)Counting objects:  77% (7/9)Counting objects:  88% (8/9)Counting objects: 100% (9/9)Counting objects: 100% (9/9), done.
-Delta compression using up to 2 threads
-Compressing objects:  20% (1/5)Compressing objects:  40% (2/5)Compressing objects:  60% (3/5)Compressing objects:  80% (4/5)Compressing objects: 100% (5/5)Compressing objects: 100% (5/5), done.
-Writing objects:  20% (1/5)Writing objects:  40% (2/5)Writing objects:  60% (3/5)Writing objects:  80% (4/5)Writing objects: 100% (5/5)Writing objects: 100% (5/5), 1.18 KiB | 1.18 MiB/s, done.
-Total 5 (delta 4), reused 0 (delta 0), pack-reused 0
-remote: Resolving deltas:   0% (0/4)[Kremote: Resolving deltas:  25% (1/4)[Kremote: Resolving deltas:  50% (2/4)[Kremote: Resolving deltas:  75% (3/4)[Kremote: Resolving deltas: 100% (4/4)[Kremote: Resolving deltas: 100% (4/4), completed with 4 local objects.[K
-To github.com:ScottieTee/Card-Game.git
-   e5f968a..64d1d5a  main -> main
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hggit ad   g  nnode script.s js[?2004l
-]633;C]633;E;node script.jsnode:internal/modules/cjs/loader:936
-  throw err;
-  ^
-
-Error: Cannot find module '/Users/scotthompson/Desktop/Card-Game/script.js'
-[90m    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:933:15)[39m
-[90m    at Function.Module._load (node:internal/modules/cjs/loader:778:27)[39m
-[90m    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)[39m
-[90m    at node:internal/main/run_main_module:17:47[39m {
-  code: [32m'MODULE_NOT_FOUND'[39m,
-  requireStack: []
-}
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;1]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.jsnode:internal/modules/cjs/loader:936
-  throw err;
-  ^
-
-Error: Cannot find module '/Users/scotthompson/Desktop/Card-Game/script.js'
-[90m    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:933:15)[39m
-[90m    at Function.Module._load (node:internal/modules/cjs/loader:778:27)[39m
-[90m    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)[39m
-[90m    at node:internal/main/run_main_module:17:47[39m {
-  code: [32m'MODULE_NOT_FOUND'[39m,
-  requireStack: []
-}
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;1]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.s[?2004l
-]633;C]633;E;node script.snode:internal/modules/cjs/loader:936
-  throw err;
-  ^
-
-Error: Cannot find module '/Users/scotthompson/Desktop/Card-Game/script.s'
-[90m    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:933:15)[39m
-[90m    at Function.Module._load (node:internal/modules/cjs/loader:778:27)[39m
-[90m    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:77:12)[39m
-[90m    at node:internal/main/run_main_module:17:47[39m {
-  code: [32m'MODULE_NOT_FOUND'[39m,
-  requireStack: []
-}
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;1]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.jsHow many players?[s[2K[0GHow many players?1[19G
-0 [s[2K[0G0 a[4G[s[2K[0G0 ah[5G[s[2K[0G0 ah [6G[s[2K[0G0 ah q[7G[s[2K[0G0 ah qh[8G[s[2K[0G0 ah qh [9G[s[2K[0G0 ah qh 3[10G[s[2K[0G0 ah qh 3c[11G
-0
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.s js[?2004l
-]633;C]633;E;node script.jsHow many players?[s[2K[0GHow many players?1[19G
-0 [s[2K[0G0 ah [6G[6G[s[2K[0G0 ah qh [9G[9G[s[2K[0G0 ah qh 3c[11G[11G
-0
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.jsnode:internal/errors:464
-    ErrorCaptureStackTrace(err);
-    ^
-
-Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/Users/scotthompson/Desktop/Card-Game/hand.js' imported from /Users/scotthompson/Desktop/Card-Game/script.js
-[90m    at new NodeError (node:internal/errors:371:5)[39m
-[90m    at finalizeResolution (node:internal/modules/esm/resolve:418:11)[39m
-[90m    at moduleResolve (node:internal/modules/esm/resolve:983:10)[39m
-[90m    at defaultResolve (node:internal/modules/esm/resolve:1080:11)[39m
-[90m    at ESMLoader.resolve (node:internal/modules/esm/loader:530:30)[39m
-[90m    at ESMLoader.getModuleJob (node:internal/modules/esm/loader:251:18)[39m
-[90m    at ModuleWrap.<anonymous> (node:internal/modules/esm/module_job:79:40)[39m
-[90m    at link (node:internal/modules/esm/module_job:78:36)[39m {
-  code: [32m'ERR_MODULE_NOT_FOUND'[39m
-}
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;1]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.js[?2004l
-]633;C]633;E;node script.jsnode:internal/process/esm_loader:94
-    internalBinding('errors').triggerUncaughtException(
-                              ^
-
-Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/Users/scotthompson/Desktop/Card-Game/JS/hand' imported from /Users/scotthompson/Desktop/Card-Game/script.js
-Did you mean to import ../JS/hand.js?
-[90m    at new NodeError (node:internal/errors:371:5)[39m
-[90m    at finalizeResolution (node:internal/modules/esm/resolve:418:11)[39m
-[90m    at moduleResolve (node:internal/modules/esm/resolve:983:10)[39m
-[90m    at defaultResolve (node:internal/modules/esm/resolve:1080:11)[39m
-[90m    at ESMLoader.resolve (node:internal/modules/esm/loader:530:30)[39m
-[90m    at ESMLoader.getModuleJob (node:internal/modules/esm/loader:251:18)[39m
-[90m    at ModuleWrap.<anonymous> (node:internal/modules/esm/module_job:79:40)[39m
-[90m    at link (node:internal/modules/esm/module_job:78:36)[39m {
-  code: [32m'ERR_MODULE_NOT_FOUND'[39m
-}
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;1]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004hnnode script.s js[?2004l
-]633;C]633;E;node script.jsHow many players?[s[2K[0GHow many players?1[19G
-0 [s[2K[0G0 a[4G[2D[s[2K[0G0 [3G[s[2K[0G0 A[4G[s[2K[0G0 Ah[5G[s[2K[0G0 Ah [6G[s[2K[0G0 Ah 2[7G[s[2K[0G0 Ah 2c[8G[s[2K[0G0 Ah 2c [9G[s[2K[0G0 Ah 2c 4[10G[s[2K[0G0 Ah 2c 4d[11G
-0
-[1m[7m%[27m[1m[0m                                                                                                                                                   ]633;D;0]633;P;Cwd=/Users/scotthompson/Desktop/Card-Game[0m[27m[24m[J]633;Ascotthompson@Scots-MacBook-Pro Card-Game % ]633;B[K[?2004h
